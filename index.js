ceb18ac36402e741a30e54c5e376f81d8e5d09b0
Bill2093 <pedroluisoa20@gmail.com>
2024-11-29 15:44:01 -0500

    modificando atawues

--- index.js
+++ index.js
@@ -9,16 +9,31 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color = 'red'}) {
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: this.position ,
+            width: 100,
+            height: 50, 
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        // attack box
+        c.fillStyle = 'green'
+        c.fillRect(this.attackBox.position.x, 
+                   this.attackBox.position.y, 
+                   this.attackBox.width, 
+                   this.attackBox.height)
     }
 
     update(){
@@ -29,6 +44,14 @@ class Sprite {
         if(this.position.y + this.height + this.velocity.y >= canvas.height){
             this.velocity.y = 0
         } else this.velocity.y += gravity
+
+
+    }
+    attack() {
+        this.isAttacking = true
+        setTimeout(()=>{
+        this.isAttacking = false
+        }, 100)
     }
 }
 
@@ -40,7 +63,9 @@ const player = new Sprite({
 velocity: {
     x:0,
     y:0
-}})
+},
+color: 'blue'
+})
 
 
 
@@ -98,6 +123,17 @@ function animate(){
     enemy.velocity.x = 5;
     }
 
+    // detect for collision
+    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && 
+        player.attackBox.position.x <= enemy.position.x + enemy.width && 
+        player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+        && player.attackBox.position.y <= enemy.position.y + enemy.height && 
+        player.isAttacking
+    ){
+        player.isAttacking = false
+        console.log('go');
+    }
+
   }
 
 animate()
@@ -119,6 +155,9 @@ switch (event.key) {
     case 'ArrowRight' :
             keys.ArrowRight.pressed = true
             enemy.lastKey ='ArrowRight'
+            break
+    case ' ':
+        player.attack()
             break
     case 'ArrowLeft':
             keys.ArrowLeft.pressed = true
