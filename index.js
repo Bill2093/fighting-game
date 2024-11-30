8fcb5cefcd4037eba5e04d998c614f02fb032e78
Bill2093 <pedroluisoa20@gmail.com>
2024-11-30 15:34:24 -0500

    marcandio los ataques

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity, color = 'red'}) {
+    constructor({position, velocity, color = 'red', offset}) {
         this.position = position
         this.velocity = velocity
         this.width = 50
         this.height = 150
         this.lastKey
         this.attackBox = {
-            position: this.position ,
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
             width: 100,
             height: 50, 
         }
@@ -29,16 +33,21 @@ class Sprite {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
         // attack box
+        //if (this.isAttacking) {
         c.fillStyle = 'green'
         c.fillRect(this.attackBox.position.x, 
                    this.attackBox.position.y, 
                    this.attackBox.width, 
-                   this.attackBox.height)
-    }
+                   this.attackBox.height
+        )
+    //}
+}
 
     update(){
         this.draw()
-        this.position.x += this.velocity.x
+        this.attackBox.position.x =this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y =this.position.y
+        this.position.x += this.velocity.x 
         this.position.y += this.velocity.y
 
         if(this.position.y + this.height + this.velocity.y >= canvas.height){
@@ -64,9 +73,11 @@ velocity: {
     x:0,
     y:0
 },
-color: 'blue'
+offset:{
+    x:0,
+    y:0,
+}
 })
-
 
 
 const enemy = new Sprite({
@@ -77,6 +88,11 @@ const enemy = new Sprite({
 velocity: {
     x:0,
     y:0
+},
+color: 'blue',
+offset:{
+    x:-50,
+    y:0,
 }})
 
 console.log(player)
@@ -98,6 +114,15 @@ const keys ={
         pressed: false
     }
 }
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    );
+}
+
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -124,15 +149,26 @@ function animate(){
     }
 
     // detect for collision
-    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && 
-        player.attackBox.position.x <= enemy.position.x + enemy.width && 
-        player.attackBox.position.y + player.attackBox.height >= enemy.position.y
-        && player.attackBox.position.y <= enemy.position.y + enemy.height && 
+    if (
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        }) &&
         player.isAttacking
-    ){
-        player.isAttacking = false
+    ) {
+        player.isAttacking = false;
         console.log('go');
     }
+    if (
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        }) &&
+        enemy.isAttacking
+    ) {
+        enemy.isAttacking = false;
+        console.log('genemy attack successful');
+    } 
 
   }
 
@@ -165,6 +201,9 @@ switch (event.key) {
             break
     case 'ArrowUp':
             enemy.velocity.y = -30
+            break
+    case 'ArrowDown':
+            enemy.isAttacking = true
             break
     }
 console.log(event.key)
