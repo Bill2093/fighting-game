88674a3550f94bfe8defbb20761949672222ccef
Bill2093 <pedroluisoa20@gmail.com>
2024-12-05 19:01:44 -0500

    funciona el mensaje cuando se acaban las vidas

--- index.js
+++ index.js
@@ -124,12 +124,29 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     );
 }
 
-let timer = 10
+
+function determineWinner({player, enemy}) {
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'    
+    } else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'            
+    }
+
+}
+
+let timer = 60
 function decreaseTimer() {
-    setTimeout(decreaseTimer, 1000)
     if (timer > 0) {
+        setTimeout(decreaseTimer, 1000)
         timer--
         document.querySelector('#timer').innerHTML = timer
+    }
+
+    if(timer === 0) {
+        determineWinner({player, enemy})
     }
 }
 
@@ -185,7 +202,14 @@ function animate(){
         document.querySelector('#playerHealth').style.width = player.health + '%'
     } 
 
+      // end game based on health
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWinner({player, enemy})
+ }
+
   }
+
+
 
 animate()
 
